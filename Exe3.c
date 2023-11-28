69d5b989094fb0db18047d30ca1eb6f03d7021c0
YasminRez <142317229+YasminRez@users.noreply.github.com>
2023-11-28 09:35:31 -0300

    Exe3.c

--- Exe3.c
+++ Exe3.c
@@ -1,69 +1,112 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
-
-int main()
-{
-  int matriz1[4][4],i, j;
-  int matriz2[4][4],m, n;
-  int matriz3Resultado[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-  char operacao[7];
-
-  //atribui os valores para a matriz
-  for ( i=0; i<4; i++ )
-    for ( j=0; j<4; j++ )
-    {
-
-      scanf ("%d", &matriz1[ i ][ j ]);
-    }
-  for ( m=0; m<4; m++ )
-    for ( n=0; n<4; n++ )
-    {
-
-      scanf ("%d", &matriz2[ m ][ n ]);
-    }  
-     scanf("%s", operacao);
-if(strcmp(operacao, "soma") == 0){
-    for( i=0; i<4; i++ ){
-        for( m=0; m<4; m++ ){
-            matriz3Resultado[i][m] = matriz1[i][m] + matriz2[i][m];
-        }
-    }
-    for ( i=0; i<4; i++ ){
-    for ( j=0; j<4; j++ )
-    {
-      printf ("%4d",matriz3Resultado[ i ][ j ]);
-    }
-    printf("\n");
-    }
-} else if(strcmp(operacao, "sub") == 0) {
-    for( j=0; j<4; j++ ){
-        for( n=0; n<4; n++ ){
-            matriz3Resultado[j][n] = matriz1[j][n] - matriz2[j][n];
-        }
-    }
-    for ( i=0; i<4; i++ ){
-    for ( j=0; j<4; j++ )
-    {
-      printf ("%4d",matriz3Resultado[ i ][ j ]);
-    }
-      printf("\n");
-    }
-}else{
-    for( i=0; i<4; i++ ){
-        for( j=0; j<4; j++ ){
-          for( n=0; n<4; n++ ){
-matriz3Resultado[i][j] = matriz3Resultado[i][j] + matriz1[i][n] * matriz2[n][j];
-          }
-        }
-    }
-    for ( i=0; i<4; i++ ){
-    for ( j=0; j<4; j++ )
-    {
-      printf ("%4d",matriz3Resultado[ i ][ j ]);
-    }
-     printf("\n");
-}
+#include <stdlib.h>
+int is_alpha_upper(char c) {
+    return c >= 'A' && c <= 'Z';
 }
 
-  return 0;
+int is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int is_valid_license_plate(char placa[8]) {
+   
+    if (strlen(placa) == 8 &&
+        is_alpha_upper(placa[0]) && is_alpha_upper(placa[1]) && is_alpha_upper(placa[2]) &&
+        placa[3] == '-' &&
+        is_digit(placa[4]) && is_digit(placa[5]) && is_digit(placa[6]) && is_digit(placa[7])) {
+        return 1;
+    }
+   
+    else if (strlen(placa) == 7 &&
+             is_alpha_upper(placa[0]) && is_alpha_upper(placa[1]) && is_alpha_upper(placa[2]) &&
+             is_digit(placa[3]) && is_alpha_upper(placa[4]) && is_digit(placa[5]) && is_digit(placa[6])) {
+        return 2;
+    }
+   
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    char placa[9];
+
+    char DiaSemana[20];
+    char dia1[] = "SEGUNDA-FEIRA";
+    char dia2[] = "TERCA-FEIRA";
+    char dia3[] = "QUARTA-FEIRA";
+    char dia4[] = "QUINTA-FEIRA";
+    char dia5[] = "SEXTA-FEIRA";
+    char dia6[] = "SABADO";
+    char dia7[] = "DOMINGO";
+
+    scanf("%s", placa);
+    limparBuffer();
+    scanf("%s", DiaSemana);
+
+    int result = is_valid_license_plate(placa);
+
+
+    for (int i = 0; DiaSemana[i]; i++) {
+        if (result == 0 && DiaSemana[i] >= 'a' && DiaSemana[i] <= 'z') {
+            printf("Placa invalida\n");
+            printf("Dia da semana invalido\n");
+            return 0;
+        } else if (result == 0){
+            printf("Placa invalida\n");
+            return 0;
+        } else if (DiaSemana[i] >= 'a' && DiaSemana[i] <= 'z'){
+             printf("Dia da semana invalido\n");
+            return 0;
+        }
+    }
+   
+    int ultimoNumero = placa[strlen(placa) - 1]-'0';
+    limparBuffer();
+
+    // Comparar dias da semana
+    if (strcmp(DiaSemana, dia1) == 0) {
+    if (ultimoNumero == 0 || ultimoNumero == 1) {
+        printf("%s nao pode circular segunda-feira\n", placa);
+    } else {
+        printf("%s pode circular segunda-feira\n", placa);
+    }
+} else if (strcmp(DiaSemana, dia2) == 0) {
+    if (ultimoNumero == 2 || ultimoNumero == 3) {
+        printf("%s nao pode circular terca-feira\n", placa);
+    } else {
+        printf("%s pode circular terca-feira\n", placa);
+    }
+} else if (strcmp(DiaSemana, dia3) == 0) {
+    if (ultimoNumero == 4 || ultimoNumero == 5) {
+        printf("%s nao pode circular quarta-feira\n", placa);
+    } else {
+        printf("%s pode circular quarta-feira\n", placa);
+    }
+} else if (strcmp(DiaSemana, dia4) == 0) {
+    if (ultimoNumero == 6 || ultimoNumero == 7) {
+        printf("%s nao pode circular quinta-feira\n", placa);
+    } else {
+        printf("%s pode circular quinta-feira\n", placa);
+    }
+} else if (strcmp(DiaSemana, dia5) == 0) {
+    if (ultimoNumero == 8 || ultimoNumero == 9) {
+        printf("%s nao pode circular sexta-feira\n", placa);
+    } else {
+        printf("%s pode circular sexta-feira\n", placa);
+    }
+}  else if (strcmp(DiaSemana, dia6) == 0 || strcmp(DiaSemana, dia7) == 0) {
+        printf("Nao ha proibicao no fim de semana\n");
+      } else {
+        printf("Dia da semana invalido\n");
+        return 0;
+      }
+   
+    return 0;
 }
